12f1699418f1639759884fd840e13717fc50eb0c
kodmate <kodmate@icloud.com>
2015-05-28 19:31:59 +0300

    Laravel default folder structure is updated.

---
+++ libraries/Foundation/Application.php
@@ -0,0 +1,16 @@
+<?php namespace Mate\Foundation;
+
+/**
+ * Kodmate
+ *
+ * New generation content management system.
+ *
+ * @package  Kodmate
+ * @author   Mahmut Aktas <kodmate@icloud.com>
+ */
+
+use Illuminate\Foundation\Application as ApplicationBase;
+
+class Application extends ApplicationBase
+{
+}
